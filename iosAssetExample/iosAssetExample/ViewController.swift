b9b9e318d899dd41dcbfa705d96719a8b8fc410c
polypropylene16g <polypropylene16g@gmail.com>
2023-02-21 01:11:35 +0900

    initial

---
+++ iosAssetExample/iosAssetExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosAssetExample
+//
+//  Created by N2018-0257-001-009 on 2023/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
